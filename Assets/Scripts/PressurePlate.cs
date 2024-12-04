d9e18e86272d4ca4caedc0ee72050f98ca3eb6f2
Sander Hahn <nylux.22@gmail.com>
2024-12-04 23:16:51 +0100

    added assets

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject Sensor;
+    [SerializeField]
+    bool allowPlayers = true;
+    [SerializeField]
+    bool allowHeavyFilament;
+    [SerializeField]
+    bool allowAllFilaments;
+    [SerializeField]
+    bool allowAllGameObjects;
+    [SerializeField]
+    bool stayActivated;
+
+    public bool isActive = false;
+    int items;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+
+        if (ShouldIncrement(other))
+        {
+            items++;
+            isActive = true;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!stayActivated && ShouldIncrement(other))
+        {
+            items--;
+            if (items == 0)
+            {
+                isActive = false;
+            }
+        }
+    }
+
+    private bool ShouldIncrement(Collider other)
+    {
+        if (allowAllGameObjects) return true;
+        if (allowPlayers && other.CompareTag("Player")) return true;
+        if (other.TryGetComponent<CustomTags>(out var customTags))
+        {
+            if (allowAllFilaments && customTags.HasTag("Filament")) return true;
+            if (allowHeavyFilament && customTags.HasTag("Heavy")) return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/SensorChangeMaterial.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorChangeMaterial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool active;
+    public Material oldmaterial;
+    public Material newmaterial;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ToggleMaterial();
+    }
+
+    void ToggleMaterial()
+    {
+        if (active)
+        {
+            GetComponent<Renderer>().material = newmaterial;
+        }
+        else 
+        { 
+            GetComponent<Renderer>().material = oldmaterial;
+        }
+    }
+}
